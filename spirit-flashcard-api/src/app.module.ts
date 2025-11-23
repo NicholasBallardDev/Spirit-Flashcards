e44256ea22f82ae5243a9bfb5720c693cf94ea04
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2025-11-23 15:24:52 +1100

    Added card and flashcard deck modules

--- spirit-flashcard-api/src/app.module.ts
+++ spirit-flashcard-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CardModule } from './card/card.module';
+import { FlashcardDeckModule } from './flashcard-deck/flashcard-deck.module';
 
 @Module({
-  imports: [],
+  imports: [CardModule, FlashcardDeckModule],
   controllers: [AppController],
   providers: [AppService],
 })
