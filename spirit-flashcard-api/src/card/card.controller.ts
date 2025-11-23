3c830b6e8e4316f0560f92f2e6622bee2f65f10a
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2025-11-23 16:36:01 +1100

    Added scaffold for card controller, installed typeorm and postgre sql

--- spirit-flashcard-api/src/card/card.controller.ts
+++ spirit-flashcard-api/src/card/card.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('card')
-export class CardController {}
+export class CardController {
+    @Get()
+    getCard(){
+        return "This is a card";
+    }
+
+    @Post()
+    addCard(){
+        return "Card has been added";
+    }
+
+    @Put()
+    updateCard(){
+        return "Card has been updated";
+    }
+
+    @Delete()
+    deleteCard(){
+        return "Card has been deleted";
+    }
+}
