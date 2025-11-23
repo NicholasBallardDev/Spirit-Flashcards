87adfd9627d5a11c0332d30818a4c9d2a214886b
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2025-11-23 17:33:01 +1100

    Connected Application to Postgres Database

--- spirit-flashcard-api/src/app.module.ts
+++ spirit-flashcard-api/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CardModule } from './card/card.module';
 import { FlashcardDeckModule } from './flashcard-deck/flashcard-deck.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CardModule, FlashcardDeckModule],
+  imports: [CardModule, FlashcardDeckModule, 
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    entities: [],
+    synchronize: true, //remove in production environment
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
