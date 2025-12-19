aff12cc20a2691822dffa9f18e405310d56eb614
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2025-12-19 22:18:25 +1100

    Implemented schedule service

---
+++ spirit-frontend/server/services/schedule.service.ts
@@ -0,0 +1,60 @@
+import { Schedule, State, Rating } from "@/Types/schedule"
+
+const API_URL = "http://localhost:3000"
+
+export async function getSchedules(): Promise<Schedule[]> {
+  const res = await fetch(`${API_URL}/schedule`)
+  return res.json()
+}
+
+export async function getSchedule(id: number): Promise<Schedule> {
+  const res = await fetch(`${API_URL}/schedule/${id}`)
+  return res.json()
+}
+
+export async function getSchedulesByState(state: State): Promise<Schedule[]> {
+  const res = await fetch(`${API_URL}/schedule/state/${state}`)
+  return res.json()
+}
+
+export async function getSchedulesByDeckAndState(
+  deckId: number,
+  state: State
+): Promise<Schedule[]> {
+  const urlParams = new URLSearchParams(
+    {
+      state: state,
+    }.toString()
+  )
+  const res = await fetch(`${API_URL}/schedule/deck/${deckId}?` + urlParams)
+  return res.json()
+}
+
+export async function createSchedule(): Promise<Schedule> {
+  const res = await fetch(`${API_URL}/schedule`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  })
+  return res.json()
+}
+
+export async function updateSchedule(
+  id: number,
+  rating: Rating
+): Promise<Schedule> {
+  const urlParams = new URLSearchParams(
+    {
+      rating: rating,
+    }.toString()
+  )
+  const res = await fetch(`${API_URL}/schedule/${id}?` + urlParams, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+  })
+
+  if (!res.ok) {
+    throw new Error(`Failed to update schedule ${id}`)
+  }
+
+  return res.json()
+}
