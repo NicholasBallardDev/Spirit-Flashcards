a799713200b7da52db97a93c36b55ad05eabb5f0
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-12 22:04:24 +1100

    Created MetricCard component, added static data to home page

--- spirit-frontend/app/page.tsx
+++ spirit-frontend/app/page.tsx
@@ -1,7 +1,36 @@
-import Image from "next/image";
+"use client"
+import { MetricCard } from "@/features/dashboard/components/MetricCard"
+import Image from "next/image"
 
 export default function Home() {
   return (
-    <h1>Flashcards Frontend</h1>
-  );
+    <div className="mt-10 flex flex-col items-center">
+      <div className="w-full max-w-6xl px-6">
+        <h1 className="ml-2 font-bold text-2xl">Overview</h1>
+        <div className="flex mt-4 gap-10">
+          <MetricCard
+            key={1}
+            number={23}
+            title={"Total Decks"}
+            subtitle={"You have quite alot to study!"}
+            onClick={() => console.log(`Navigating to food`)}
+          />
+          <MetricCard
+            key={2}
+            number={98}
+            title={"Total Cards"}
+            subtitle={"Keep up and study hard!"}
+            onClick={() => console.log(`Navigating to food`)}
+          />
+          <MetricCard
+            key={3}
+            number={52}
+            title={"Cards Due"}
+            subtitle={"Do your daily reviews!"}
+            onClick={() => console.log(`Navigating to food`)}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
---
+++ spirit-frontend/features/dashboard/components/MetricCard.tsx
@@ -0,0 +1,39 @@
+interface MetricCardProps {
+  number: number
+  title: string
+  subtitle: string
+  onClick: () => void
+}
+
+export function MetricCard({
+  number,
+  title,
+  subtitle,
+  onClick,
+}: MetricCardProps) {
+  return (
+    <div className="flex-1 min-w-0 bg-white pt-10 pb-16 pl-6 pr-14 rounded-xl border border-gray-100 shadow-sm flex flex-col justify-between relative h-full">
+      <div className="flex gap-4">
+        <div
+          className={`flex items-center justify-center w-12 h-12 rounded-full font-bold text-xl bg-blue-50 text-blue-500`}
+        >
+          {number}
+        </div>
+
+        <div>
+          <h3 className="text-lg font-bold text-gray-900 mb-1">{title}</h3>
+          <p className="text-sm text-gray-500 leading-tight">{subtitle}</p>
+        </div>
+      </div>
+
+      <div className="absolute bottom-6 right-6">
+        <button
+          onClick={onClick}
+          className="flex items-center gap-1 text-sm font-semibold text-gray-900 hover:underline"
+        >
+          Hello !
+        </button>
+      </div>
+    </div>
+  )
+}
