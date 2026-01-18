29cc8cdf139c97991ad79685423cf840428835df
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-18 16:57:40 +1100

    Edited CreateDeckDialogue: -Redirect user to deck edit screen after creating a new deck

--- spirit-frontend/features/deck/components/CreateDeckDialogue.tsx
+++ spirit-frontend/features/deck/components/CreateDeckDialogue.tsx
@@ -3,22 +3,26 @@
 import { createDeck } from "@/server/services/deck.service"
 import { toast, Toaster } from "sonner"
 import { DeckFormDialog } from "./DeckFormDialogue"
+import { useRouter } from "next/navigation"
 
 interface CreateDeckDialogProps {
   trigger: React.ReactNode
 }
 
 export function CreateDeckDialog({ trigger }: CreateDeckDialogProps) {
+  const router = useRouter()
+
   const handleSubmit = async (values: {
     name: string
     description: string
   }) => {
-    await createDeck({
+    const newDeck = await createDeck({
       name: values.name,
       description: values.description,
       cards: [],
     })
     toast.success("Deck successfully created")
+    router.push(`/decks/${newDeck.id}/edit`)
   }
 
   return (
