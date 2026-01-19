fd927a5a809f9e4f36d8c9b6950f050787c948f5
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-19 17:18:34 +1100

    Created deckContext

---
+++ spirit-frontend/features/deck/context/deckContext.tsx
@@ -0,0 +1,27 @@
+import { FlashcardDeck } from "@/Types"
+import { createContext, useContext, ReactNode } from "react"
+
+interface DeckContextType {
+  onDelete: (id: number) => Promise<void>
+}
+
+export const DeckContext = createContext<DeckContextType | undefined>(undefined)
+
+export function useDeckContext() {
+  const context = useContext(DeckContext)
+
+  if (!context) {
+    throw new Error("useDeckContext must be used within a DeckProvider")
+  }
+
+  return context
+}
+
+export function DeckProvider({
+  children,
+  onDelete,
+}: DeckContextType & { children: ReactNode }) {
+  return (
+    <DeckContext.Provider value={{ onDelete }}>{children}</DeckContext.Provider>
+  )
+}
