bf04cfd6ae57b51ac481d17f2428b731b945977d
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-20 00:00:50 +1100

    Made study client larger

--- spirit-frontend/app/decks/[id]/study/page.tsx
+++ spirit-frontend/app/decks/[id]/study/page.tsx
@@ -1,16 +1,18 @@
-import { getDeck } from "@/server/services/deck.service";
-import { StudyClient } from "./StudyClient";
+import { getDeck } from "@/server/services/deck.service"
+import { StudyClient } from "./StudyClient"
 
-export default async function StudyPage({ params }: {
-    params: Promise<{id: string}>
+export default async function StudyPage({
+  params,
+}: {
+  params: Promise<{ id: string }>
 }) {
-    const deckId = parseInt((await params).id);
-    const deck = await getDeck(deckId);
-    return (
-        <div className="flex justify-center">
-            <div className="h-[50vh] w-1/2">
-                <StudyClient deck={deck}/>
-            </div>
-        </div>
-    );
+  const deckId = parseInt((await params).id)
+  const deck = await getDeck(deckId)
+  return (
+    <div className="flex justify-center">
+      <div className="h-[50vh] w-2/3">
+        <StudyClient deck={deck} />
+      </div>
+    </div>
+  )
 }
