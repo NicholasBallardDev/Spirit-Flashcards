4fcda8d0fd02482781b6d391573b8fb9ce03a4dc
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-21 19:33:33 +1100

    updated imports in app module in the backend

--- spirit-flashcard-api/src/app.module.ts
+++ spirit-flashcard-api/src/app.module.ts
@@ -10,26 +10,32 @@ import { FlashcardDeck } from './flashcard-deck/flashcard-deck.entity';
 import { ScheduleModule } from './schedule/schedule.module';
 import { Schedule } from './schedule/card-schedule.entity';
 import { ImagesModule } from './images/images.module';
-import { ImagesModule } from './images/images.module';
+import { Image } from './images/entities/image.entity';
 
 const DEFAULT_DB_PORT = 5432;
 
 @Module({
-  imports: [CardModule, FlashcardDeckModule, ScheduleModule,
+  imports: [
+    CardModule,
+    FlashcardDeckModule,
+    ScheduleModule,
+    ImagesModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-    type: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : DEFAULT_DB_PORT,
-    entities: [Card, FlashcardDeck, Schedule],
-    synchronize: true, 
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-  }),
-    ImagesModule],
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT
+        ? parseInt(process.env.DB_PORT)
+        : DEFAULT_DB_PORT,
+      entities: [Card, FlashcardDeck, Schedule, Image],
+      synchronize: true,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
