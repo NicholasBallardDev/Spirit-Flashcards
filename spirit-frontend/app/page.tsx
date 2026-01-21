d04f8e8c699922873b9c2bfe12dcc3d595f34b75
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-01-21 20:10:12 +1100

    removed unecessary imports from page.tsx

--- spirit-frontend/app/page.tsx
+++ spirit-frontend/app/page.tsx
@@ -2,6 +2,8 @@ import DeckCreationPrompt from "@/features/dashboard/components/DeckCreationProm
 import { MetricCard } from "@/features/dashboard/components/MetricCard"
 import MetricTray from "@/features/dashboard/components/MetricTray"
 import RecentDeckGroup from "@/features/dashboard/components/RecentDeckGroup"
+import { ImageButton } from "@/features/universal/components/ImageButton"
+import { ImageUploader } from "@/features/universal/components/ImageUploader"
 export default function Home() {
   return (
     <>
