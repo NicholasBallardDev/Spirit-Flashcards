00d78014926c3cc8e1b4448255f18dd93454f1a5
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-02-04 14:56:48 +1100

    added documentation to useIsMounted

--- spirit-frontend/features/universal/utils/useIsMounted.tsx
+++ spirit-frontend/features/universal/utils/useIsMounted.tsx
@@ -1,7 +1,17 @@
 import { useCallback, useEffect, useRef } from "react"
 
-export default function useIsMounted(initial = false) {
-  const isMounted = useRef(initial)
+/**
+ * Returns a function that returns the current mount state. This hook is useful when you have to
+ * detect component mount state within async effects.
+ *
+ * Implementation directly copied from: https://github.com/react-hookz/web/blob/master/src/useIsMounted/index.ts
+ *
+ * @param initialValue Initial value.
+ *
+ * @return Function that returns `true` only if the component is mounted.
+ */
+export function useIsMounted(initialValue = false): () => boolean {
+  const isMounted = useRef(initialValue)
   const get = useCallback(() => isMounted.current, [])
 
   useEffect(() => {
