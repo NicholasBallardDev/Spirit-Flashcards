d5ee7b83d4e2d3b102462a1eaca9160f6fd94b87
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-02-04 14:57:41 +1100

    Made image upload modal scrollable, and added next config image paths

--- spirit-frontend/features/universal/components/ImageUploadModal.tsx
+++ spirit-frontend/features/universal/components/ImageUploadModal.tsx
@@ -14,6 +14,7 @@ import { ImageUploader } from "./ImageUploader"
 import { ReactNode } from "react"
 import { UploadDropZone } from "./UploadDropZone"
 import { Card } from "@/Types"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 interface ImageUploadModalProps {
   card: Card
@@ -32,7 +33,7 @@ export const ImageUploadModal = ({
       <DialogTrigger asChild>{children}</DialogTrigger>
 
       {/* The Modal Content */}
-      <DialogContent className="sm:max-w-xl p-0 gap-0 overflow-hidden bg-white p-6 pb-0">
+      <DialogContent>
         <DialogHeader className="mb-4">
           <DialogTitle className="text-xl font-bold text-gray-900">
             Upload Image
@@ -41,38 +42,46 @@ export const ImageUploadModal = ({
             Add images to your flashcard
           </DialogDescription>
         </DialogHeader>
+        <ScrollArea className="h-[300px] pr-4">
+          {/* Question Upload Dropzone */}
+          <h3 className="text-md font-semibold mb-2">Question</h3>
+          <ImageUploader
+            cardId={card.id}
+            imageType="questionImage"
+            onUploadComplete={onCardChange}
+          >
+            {card.questionImage ? (
+              <Image
+                width={300}
+                height={300}
+                src={card.questionImage.url}
+                alt={card.questionImage.filename}
+              />
+            ) : (
+              <UploadDropZone />
+            )}
+          </ImageUploader>
 
-        {/* Question Upload Dropzone */}
-        <h3 className="text-md font-semibold mb-2">Question</h3>
-        <ImageUploader
-          cardId={card.id}
-          imageType="questionImage"
-          onUploadComplete={onCardChange}
-        >
-          {card.questionImage ? (
-            <Image
-              src={card.questionImage.url}
-              alt={card.questionImage.filename}
-            />
-          ) : (
-            <UploadDropZone />
-          )}
-        </ImageUploader>
-
-        {/* Answer Upload Dropzone */}
-        <h3 className="text-md font-semibold mb-2">Answer</h3>
-        <ImageUploader
-          cardId={card.id}
-          imageType="answerImage"
-          onUploadComplete={onCardChange}
-        >
-          {card.answerImage ? (
-            <Image src={card.answerImage.url} alt={card.answerImage.filename} />
-          ) : (
-            <UploadDropZone />
-          )}
-        </ImageUploader>
-
+          {/* Answer Upload Dropzone */}
+          <h3 className="text-md font-semibold mb-2">Answer</h3>
+          <ImageUploader
+            cardId={card.id}
+            imageType="answerImage"
+            onUploadComplete={onCardChange}
+          >
+            {card.answerImage ? (
+              <Image
+                width={200}
+                height={160}
+                src={card.answerImage.url}
+                alt={card.answerImage.filename}
+                className="w-full h-40 object-cover rounded-xl"
+              />
+            ) : (
+              <UploadDropZone />
+            )}
+          </ImageUploader>
+        </ScrollArea>
         {/* Footer Actions */}
         <DialogFooter className="p-6 bg-gray-50/50 mt-4 sm:justify-end gap-3 border-t border-gray-100">
           <DialogClose asChild>
