a9849bd5e1e8f33d66b8eedb53930f3c14915f7a
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-02-08 19:52:45 +1100

    Implemented try catch blocks for card service

--- spirit-flashcard-api/src/card/card.service.ts
+++ spirit-flashcard-api/src/card/card.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Card } from './card.entity';
 import { LessThanOrEqual, Repository } from 'typeorm';
@@ -11,6 +15,7 @@ import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import sharp from 'sharp';
 import dotenv from 'dotenv';
 import crypto from 'crypto';
+import 'multer';
 
 dotenv.config();
 
@@ -40,75 +45,107 @@ export class CardService {
   ) {}
 
   async findAll() {
-    const cards = await this.cardRepository.find({
-      relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
-    });
-    return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+    try {
+      const cards = await this.cardRepository.find({
+        relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
+      });
+      return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async countCards() {
-    return this.cardRepository.count();
+    try {
+      return await this.cardRepository.count();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async findOne(id: number) {
-    const card = await this.cardRepository.findOne({
-      where: { id },
-      relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
-    });
+    try {
+      const card = await this.cardRepository.findOne({
+        where: { id },
+        relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
+      });
 
-    return card ? this.imageService.signCard(card) : null;
+      return card ? this.imageService.signCard(card) : null;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async findDeck(deckId: number) {
-    const cards = await this.cardRepository.find({
-      where: { deck: { id: deckId } },
-      relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
-    });
-    return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+    try {
+      const cards = await this.cardRepository.find({
+        where: { deck: { id: deckId } },
+        relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
+      });
+      return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async findAllDue() {
-    const cards = await this.cardRepository.find({
-      where: {
-        schedule: {
-          due: LessThanOrEqual(new Date()),
+    try {
+      const cards = await this.cardRepository.find({
+        where: {
+          schedule: {
+            due: LessThanOrEqual(new Date()),
+          },
         },
-      },
-      relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
-    });
-    return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+        relations: ['deck', 'schedule', 'questionImage', 'answerImage'],
+      });
+      return Promise.all(cards.map((card) => this.imageService.signCard(card)));
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async countCardsDue() {
-    return this.cardRepository.count({
-      where: {
-        schedule: {
-          due: LessThanOrEqual(new Date()),
+    try {
+      return await this.cardRepository.count({
+        where: {
+          schedule: {
+            due: LessThanOrEqual(new Date()),
+          },
         },
-      },
-    });
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async create(dto: CreateCardDTO) {
-    const schedule = this.scheduleService.create();
+    try {
+      const schedule = this.scheduleService.create();
 
-    const card = this.cardRepository.create({
-      question: dto.question,
-      answer: dto.answer,
-      deck: dto.deckId ? ({ id: dto.deckId } as FlashcardDeck) : undefined,
-      schedule: schedule,
-    });
-    const saved = await this.cardRepository.save(card);
-    return saved;
+      const card = this.cardRepository.create({
+        question: dto.question,
+        answer: dto.answer,
+        deck: dto.deckId ? ({ id: dto.deckId } as FlashcardDeck) : undefined,
+        schedule: schedule,
+      });
+      const saved = await this.cardRepository.save(card);
+      return saved;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async update(id: number, dto: UpdateCardDTO) {
-    await this.cardRepository.update(id, {
-      question: dto.question,
-      answer: dto.answer,
-      deck: dto.deckId ? ({ id: dto.deckId } as FlashcardDeck) : undefined,
-    });
-    return this.findOne(id);
+    try {
+      await this.cardRepository.update(id, {
+        question: dto.question,
+        answer: dto.answer,
+        deck: dto.deckId ? ({ id: dto.deckId } as FlashcardDeck) : undefined,
+      });
+      return await this.findOne(id);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   private async setImage(
@@ -116,72 +153,94 @@ export class CardService {
     imageType: 'questionImage' | 'answerImage',
     file: Express.Multer.File,
   ) {
-    const card = await this.findOne(id);
+    try {
+      const card = await this.findOne(id);
 
-    if (!card) {
-      throw new NotFoundException(`Card with ID "${id}" not found.`);
+      if (!card) {
+        throw new NotFoundException(`Card with ID "${id}" not found.`);
+      }
+
+      const buffer = await sharp(file.buffer)
+        .resize({ height: 1080, width: 1920, fit: 'contain' })
+        .webp({ quality: 75 })
+        .toBuffer();
+
+      const key = randomImageName();
+      const webpMimeType = 'image/webp';
+      const webpFile = {
+        ...file,
+        buffer: buffer,
+        size: buffer.length,
+        originalname: `${file.originalname.replace(/\.[^/.]+$/, '')}.webp`,
+        mimetype: webpMimeType,
+      };
+
+      if (card[imageType]) {
+        // If an image already exists, update it
+        await this.imageService.update(card[imageType].id, webpFile, key);
+      } else {
+        // Otherwise, create a new one and link it
+        card[imageType] = await this.imageService.create(webpFile, key);
+        await this.cardRepository.save(card);
+      }
+
+      const params = {
+        Bucket: bucketName,
+        Key: key,
+        Body: buffer,
+        ContentType: webpMimeType,
+      };
+
+      const command = new PutObjectCommand(params);
+      await s3.send(command);
+      return await this.findOne(id);
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
     }
-
-    const buffer = await sharp(file.buffer)
-      .resize({ height: 1080, width: 1920, fit: 'contain' })
-      .webp({ quality: 75 })
-      .toBuffer();
-
-    const key = randomImageName();
-    const webpMimeType = 'image/webp';
-    const webpFile = {
-      ...file,
-      buffer: buffer,
-      size: buffer.length,
-      originalname: `${file.originalname.replace(/\.[^/.]+$/, '')}.webp`,
-      mimetype: webpMimeType,
-    };
-
-    if (card[imageType]) {
-      // If an image already exists, update it
-      await this.imageService.update(card[imageType].id, webpFile, key);
-    } else {
-      // Otherwise, create a new one and link it
-      card[imageType] = await this.imageService.create(webpFile, key);
-      await this.cardRepository.save(card);
-    }
-
-    const params = {
-      Bucket: bucketName,
-      Key: key,
-      Body: buffer,
-      ContentType: webpMimeType,
-    };
-
-    const command = new PutObjectCommand(params);
-    await s3.send(command);
-    return this.findOne(id);
   }
 
   async setQuestionImage(id: number, file: Express.Multer.File) {
-    return this.setImage(id, 'questionImage', file);
+    try {
+      return await this.setImage(id, 'questionImage', file);
+    } catch (error) {
+      throw error;
+    }
   }
 
   async setAnswerImage(id: number, file: Express.Multer.File) {
-    return this.setImage(id, 'answerImage', file);
+    try {
+      return await this.setImage(id, 'answerImage', file);
+    } catch (error) {
+      throw error;
+    }
   }
 
   async delete(id: number) {
-    const card = await this.findOne(id);
-    if (!card) {
-      throw new NotFoundException(`Item with the id of ${id} was not found`);
+    try {
+      const card = await this.findOne(id);
+      if (!card) {
+        throw new NotFoundException(`Item with the id of ${id} was not found`);
+      }
+
+      const result = await this.cardRepository.delete(id);
+
+      // Delete images when card is gone
+      if (card.questionImage)
+        await this.imageService.delete(card.questionImage.id);
+      if (card.answerImage) await this.imageService.delete(card.answerImage.id);
+
+      return {
+        message: `Deletion Successful: Item with the id of ${id} was deleted`,
+        res: result,
+      };
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
     }
-
-    const result = await this.cardRepository.delete(id);
-
-    // Delete images when card is gone
-    if (card.questionImage)
-      await this.imageService.delete(card.questionImage.id);
-    if (card.answerImage) await this.imageService.delete(card.answerImage.id);
-
-    return {
-      message: `Deletion Successful: Item with the id of ${id} was deleted`,
-      res: result,
-    };
   }
 }
