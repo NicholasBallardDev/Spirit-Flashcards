cd8df2e9c4a73ef359d58569e5dd1c59c8ba2e1b
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-02-08 19:57:59 +1100

    Implemented try catch blocks for image service

--- spirit-flashcard-api/src/images/images.service.ts
+++ spirit-flashcard-api/src/images/images.service.ts
@@ -1,13 +1,13 @@
 import {
   BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Image } from './entities/image.entity';
 import {
-  Bucket$,
   DeleteObjectCommand,
   GetObjectCommand,
   S3Client,
@@ -39,37 +39,56 @@ export class ImagesService {
   ) {}
 
   async create(file: Express.Multer.File, key: string): Promise<Image> {
-    const newImage = this.imageRepository.create({
-      filename: file.originalname,
-      key: key,
-    });
-    return this.imageRepository.save(newImage);
+    try {
+      const newImage = this.imageRepository.create({
+        filename: file.originalname,
+        key: key,
+      });
+      return await this.imageRepository.save(newImage);
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
-  findAll() {
-    return this.imageRepository.find();
+  async findAll() {
+    try {
+      return await this.imageRepository.find();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async findOne(id: number): Promise<Image> {
-    const image = await this.imageRepository.findOne({ where: { id } });
+    try {
+      const image = await this.imageRepository.findOne({ where: { id } });
 
-    if (!image) {
-      throw new NotFoundException(`Image with ID #${id} not found.`);
+      if (!image) {
+        throw new NotFoundException(`Image with ID #${id} not found.`);
+      }
+
+      image.url = await this.generateUrl(image);
+
+      return image;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
     }
-
-    image.url = await this.generateUrl(image);
-
-    return image;
   }
 
   async generateUrl(image: Image): Promise<string> {
-    const getObjectParams = {
-      Bucket: bucketName,
-      Key: image?.key,
-    };
+    try {
+      const getObjectParams = {
+        Bucket: bucketName,
+        Key: image?.key,
+      };
 
-    const command = new GetObjectCommand(getObjectParams);
-    return getSignedUrl(s3, command, { expiresIn: 3600 });
+      const command = new GetObjectCommand(getObjectParams);
+      return await getSignedUrl(s3, command, { expiresIn: 3600 });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async update(
@@ -77,45 +96,90 @@ export class ImagesService {
     newFile: Express.Multer.File,
     newKey: string,
   ): Promise<Image> {
-    this.deleteFromStorage(id);
-    const imageToUpdate = await this.findOne(id);
-    imageToUpdate.filename = newFile.originalname;
-    imageToUpdate.key = newKey;
-    const updatedImage = await this.imageRepository.save(imageToUpdate);
+    try {
+      const imageToUpdate = await this.imageRepository.findOne({
+        where: { id },
+      });
+      if (!imageToUpdate) {
+        throw new NotFoundException(`Image with ID #${id} not found.`);
+      }
 
-    return updatedImage;
+      // Delete old file from storage
+      const params = {
+        Bucket: bucketName,
+        Key: imageToUpdate.key,
+      };
+      const command = new DeleteObjectCommand(params);
+      await s3.send(command);
+
+      // Update and save
+      imageToUpdate.filename = newFile.originalname;
+      imageToUpdate.key = newKey;
+      const updatedImage = await this.imageRepository.save(imageToUpdate);
+
+      return updatedImage;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async delete(id: number): Promise<void> {
-    this.deleteFromStorage(id);
-    await this.imageRepository.delete(id);
+    try {
+      await this.deleteFromStorage(id);
+      await this.imageRepository.delete(id);
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof BadRequestException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
   }
 
   async deleteFromStorage(id: number): Promise<void> {
-    const image = await this.findOne(id);
-    if (!image) {
-      throw new BadRequestException('Image not found', {
-        cause: new Error(),
-        description: `image with id ${id} does not exist`,
-      });
+    try {
+      const image = await this.findOne(id);
+      if (!image) {
+        throw new BadRequestException('Image not found', {
+          cause: new Error(),
+          description: `image with id ${id} does not exist`,
+        });
+      }
+
+      const params = {
+        Bucket: bucketName,
+        Key: image.key,
+      };
+
+      const command = new DeleteObjectCommand(params);
+      await s3.send(command);
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof BadRequestException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
     }
-
-    const params = {
-      Bucket: bucketName,
-      Key: image.key,
-    };
-
-    const command = new DeleteObjectCommand(params);
-    await s3.send(command);
   }
 
   async signCard(card: Card): Promise<Card> {
-    if (card.questionImage) {
-      card.questionImage.url = await this.generateUrl(card.questionImage);
+    try {
+      if (card.questionImage) {
+        card.questionImage.url = await this.generateUrl(card.questionImage);
+      }
+      if (card.answerImage) {
+        card.answerImage.url = await this.generateUrl(card.answerImage);
+      }
+      return card;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
     }
-    if (card.answerImage) {
-      card.answerImage.url = await this.generateUrl(card.answerImage);
-    }
-    return card;
   }
 }
