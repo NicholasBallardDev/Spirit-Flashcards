1b9f4c8f527aa948dab0eba09094372e79765ade
NicholasBallardDev <nb.nicholas.ballard@gmail.com>
2026-02-17 16:59:42 +1100

    Implemented FlashcardDeckService definition test

--- spirit-flashcard-api/src/flashcard-deck/flashcard-deck.service.spec.ts
+++ spirit-flashcard-api/src/flashcard-deck/flashcard-deck.service.spec.ts
@@ -1,13 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FlashcardDeckService } from './flashcard-deck.service';
 import { describe, beforeEach, it, expect } from '@jest/globals';
+import { ImagesService } from '@src/images/images.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { FlashcardDeck } from './flashcard-deck.entity';
+import { Card } from '@src/card/card.entity';
 
 describe('FlashcardDeckService', () => {
   let service: FlashcardDeckService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FlashcardDeckService],
+      providers: [
+        FlashcardDeckService,
+        {
+          provide: getRepositoryToken(FlashcardDeck),
+          useValue: {}, // Mock methods for FlashcardDeck repository
+        },
+        {
+          provide: getRepositoryToken(Card),
+          useValue: {}, // Mock methods for Card repository
+        },
+        {
+          provide: ImagesService,
+          useValue: {}, // Mock methods for ImagesService
+        },
+      ],
     }).compile();
 
     service = module.get<FlashcardDeckService>(FlashcardDeckService);
